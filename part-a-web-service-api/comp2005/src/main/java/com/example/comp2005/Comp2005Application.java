5ae942028fa4251f561d5af1ed3ccd3d489154f1
S <sph.lvstz@gmail.com>
2026-03-26 21:14:06 +0000

    initialise spring boot project

---
+++ part-a-web-service-api/comp2005/src/main/java/com/example/comp2005/Comp2005Application.java
@@ -0,0 +1,13 @@
+package com.example.comp2005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp2005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp2005Application.class, args);
+	}
+
+}
---
+++ part-a-web-service-api/comp2005/src/test/java/com/example/comp2005/Comp2005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comp2005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp2005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
